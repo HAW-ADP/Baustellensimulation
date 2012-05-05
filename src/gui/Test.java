ed65ba110deaf18b3fb44883e5685a41a9dee23a
D. Fleischhauer <D.Fleischhauer@gmx.de>
2012-05-05 23:30:43 +0200

    der Zustand ist im Applet und funktioniert

--- src/gui/Test.java
+++ src/gui/Test.java
@@ -1,5 +1,11 @@
 package gui;
 
+import gui.logik.Uhrzeit;
+import gui.logik.UhrzeitImpl;
+import gui.logik.Zustand;
+import gui.logik.ZustandImpl;
+import java.util.PriorityQueue;
+import java.util.Random;
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -24,15 +30,36 @@ public class Test extends PApplet {
     final private int AUSFAHRTAMPELX = 90;
     final private int AUSFAHRTAMPELY = 240;
     // Fixe Größen
-    final private int ANZAHLAUTOSBAUSTELLE = 3;
-    final private int ANZAHLAUTOSPARKHAUSZEILE = 20;
-    final private int ANZAHLAUTOSPARKHAUSSPALTE = 10;
-    final private int ANZAHLAUTOSEINFAHRT = 5;
-    final private int ANZAHLAUTOSAUSFAHRT = 7;
+    final private int BAUSTELLEKAPAZITAET = 3;
+    final private int PARKHAUSZEILEKAPAZITAET = 20;
+    final private int PARKHAUSSPALTEKAPAZITAET = 10;
+    final private int EINFAHRTKAPAZITAET = 5;
+    final private int AUSFAHRTKAPAZITAET = 7;
     private PImage welt = loadImage("src\\gui\\img\\welt.jpg");
     private PImage weltUnten = loadImage("src\\gui\\img\\weltUnten.jpg");
     private PImage auto1 = loadImage("src\\gui\\img\\auto1.jpg");
     private PImage auto2 = loadImage("src\\gui\\img\\auto2.jpg");
+    //Daten der Simulation
+    Uhrzeit minAutoAnkunft = new UhrzeitImpl(0, 0, 30);
+    Uhrzeit maxAutoAnkunft = new UhrzeitImpl(0, 2, 0);
+    Random rand = new Random();
+    int minAnkunft = minAutoAnkunft.gesamtZeitSekunden();
+    int maxAnkunft = maxAutoAnkunft.gesamtZeitSekunden();
+    int naechstesAutoEinfahrtInt = minAnkunft + rand.nextInt(maxAnkunft - minAnkunft);
+    Uhrzeit startzeit = new UhrzeitImpl(10, 0, 0);
+    Uhrzeit naechstesAutoEinfahrt = new UhrzeitImpl(naechstesAutoEinfahrtInt).addiere(startzeit);
+    Uhrzeit autoAbstand = new UhrzeitImpl(3);
+    Uhrzeit baustellenZeit = new UhrzeitImpl(20);
+    Uhrzeit minParkDauer = new UhrzeitImpl(0, 30, 0);
+    Uhrzeit maxParkDauer = new UhrzeitImpl(1, 0, 0);
+    int parkplatzKapazitaet = 100;
+    int einfahrtKapazitaet = 10;
+    Uhrzeit maxRot = new UhrzeitImpl(0, 2, 0);
+    Zustand zustand = new ZustandImpl(0, 0, Zustand.STOP_AUSFAHRT, naechstesAutoEinfahrt,
+            startzeit, new PriorityQueue<Uhrzeit>(), new PriorityQueue<Uhrzeit>(),
+            naechstesAutoEinfahrt, new UhrzeitImpl(0, 0, 0), autoAbstand, baustellenZeit,
+            minParkDauer, maxParkDauer, minAutoAnkunft, maxAutoAnkunft, maxRot,
+            PARKHAUSSPALTEKAPAZITAET * PARKHAUSZEILEKAPAZITAET, EINFAHRTKAPAZITAET);
     private float demoAuto1PosX = 500;
     private float demoAuto1PosY = 471;
     private float demoAuto2PosX = 0;
@@ -43,7 +70,6 @@ public class Test extends PApplet {
     private int posBaustelle = 0;
     private String ampelEinfahrt = "R";
     private String ampelAusfahrt = "R";
-    private int ampelZustand = 1;
 
     @Override
     public void setup() {
@@ -51,22 +77,6 @@ public class Test extends PApplet {
         background(255);
         frameRate(30);
         image(welt, 0, 0);
-        image(weltUnten, 0, 469);
-
-        // Parkplatz mit 10x10 Pl�tzen
-        zeichneRaster(PARKHAUSX, PARKHAUSY, ANZAHLAUTOSPARKHAUSSPALTE, ANZAHLAUTOSPARKHAUSZEILE);
-        // Warteschlange vor der Ausfahrt
-        zeichneRaster(AUSFAHRTWARTEBEREICHX, AUSFAHRTWARTEBEREICHY, ANZAHLAUTOSAUSFAHRT, 1);
-        // Wartebereich vor der Einfahrt
-        zeichneRaster(EINFAHRTWARTEBEREICHX, EINFAHRTWARTEBEREICHY, ANZAHLAUTOSEINFAHRT, 1);
-        // Baustellenbereich
-        zeichneRaster(BAUSTELLENBEREICHX, BAUSTELLENBEREICHY, 1, ANZAHLAUTOSBAUSTELLE);
-    }
-
-    @Override
-    public void draw() {
-        stroke(0);
-        //Immer wieder laden, da das Spielfeld verwischt wird
         image(weltUnten, 0, 469);
         // Farbe f�r die Kn�pfe (WEISS)
         fill(255);
@@ -90,12 +100,26 @@ public class Test extends PApplet {
         rect(445, 5, 50, 25);
         // Knopf nächster Zustand
         rect(5, 35, 50, 25);
+        // Parkplatz mit 10x10 Pl�tzen
+        zeichneRaster(PARKHAUSX, PARKHAUSY, PARKHAUSSPALTEKAPAZITAET, PARKHAUSZEILEKAPAZITAET);
+        // Warteschlange vor der Ausfahrt
+        zeichneRaster(AUSFAHRTWARTEBEREICHX, AUSFAHRTWARTEBEREICHY, AUSFAHRTKAPAZITAET, 1);
+        // Wartebereich vor der Einfahrt
+        zeichneRaster(EINFAHRTWARTEBEREICHX, EINFAHRTWARTEBEREICHY, EINFAHRTKAPAZITAET, 1);
+        // Baustellenbereich
+        zeichneRaster(BAUSTELLENBEREICHX, BAUSTELLENBEREICHY, 1, BAUSTELLEKAPAZITAET);
+    }
+
+    @Override
+    public void draw() {
+        stroke(0);
+        //Immer wieder laden, da das Spielfeld verwischt wird
+        image(weltUnten, 0, 469);
         // Ampel f�r die Einfahrt
         zeichneAmpel(EINFAHRTAMPELX, EINFAHRTAMPELY, ampelEinfahrt);
         // Ampel f�r die Ausfahrt
         zeichneAmpel(AUSFAHRTAMPELX, AUSFAHRTAMPELY, ampelAusfahrt);
         bewegeDemoAuto();
-        // addiere Auto
     }
 
     private void zeichneAmpel(int x, int y, String farbe) {
@@ -137,50 +161,68 @@ public class Test extends PApplet {
         } else if (mouseX >= 445 && mouseX <= 495 && mouseY >= 5
                 && mouseY <= 30) {
             this.ampelWechsleZustand();
+        } else if (mouseX >= 5 && mouseX <= 55 && mouseY >= 35 && mouseY <= 60) {
+            this.zustand = this.zustand.naechsterZustand();
+            this.ampelWechsleZustand();
+            this.updateWartebereiche();
         }
     }
 
+    //Der aktuelle Ampelzustand, Einfahrt = 1, STOP_EINFAHRT = 2, AUSFAHRT = 3, STOP_AUSFAHRT = 4 
     private void ampelWechsleZustand() {
-        if (ampelZustand == 0 || ampelZustand == 2) {
-            ampelAusfahrt = "R";
-            ampelEinfahrt = "R";
-            ampelZustand++;
-        } else if (ampelZustand == 1) {
-            ampelAusfahrt = "G";
-            ampelEinfahrt = "R";
-            ampelZustand++;
-        } else if (ampelZustand == 3) {
-            ampelAusfahrt = "R";
+        if (zustand.getAmpelzustand() == 1) {
             ampelEinfahrt = "G";
-            ampelZustand++;
+        } else if (zustand.getAmpelzustand() == 2) {
+            ampelEinfahrt = "R";
+        } else if (zustand.getAmpelzustand() == 3) {
+            ampelAusfahrt = "G";
+        } else if (zustand.getAmpelzustand() == 4) {
+            ampelAusfahrt = "R";
         }
-        if (ampelZustand > 3) {
-            ampelZustand = 0;
+    }
+
+    private void updateWartebereiche() {
+        if (posEinfahrt < zustand.getEinfahrtAutos()) {
+            addiereAutos("E");
+        } else if (posEinfahrt > zustand.getEinfahrtAutos()) {
+            subtrahiereAutos("E");
+        } else if (posAusfahrt < zustand.getAusfahrtAutos()) {
+            addiereAutos("A");
+        } else if (posAusfahrt > zustand.getAusfahrtAutos()) {
+            subtrahiereAutos("A");
+        } else if (posBaustelle < zustand.getBaustellenAutos()) {
+            addiereAutos("B");
+        } else if (posBaustelle > zustand.getBaustellenAutos()) {
+            subtrahiereAutos("B");
+        } else if (posParkplatz < zustand.getParkplatzAutos()) {
+            addiereAutos("P");
+        } else if (posParkplatz > zustand.getParkplatzAutos()) {
+            subtrahiereAutos("P");
         }
     }
 
     private void addiereAutos(String wo) {
         switch (wo) {
             case "P":
-                if (this.posParkplatz < ANZAHLAUTOSPARKHAUSSPALTE * ANZAHLAUTOSPARKHAUSZEILE) {
+                if (this.posParkplatz < PARKHAUSSPALTEKAPAZITAET * PARKHAUSZEILEKAPAZITAET) {
                     this.addAutoAnPosition(PARKHAUSX, PARKHAUSY, this.posParkplatz % 10, this.posParkplatz / 10);
                     this.posParkplatz++;
                 }
                 break;
             case "E":
-                if (this.posEinfahrt < ANZAHLAUTOSEINFAHRT) {
+                if (this.posEinfahrt < EINFAHRTKAPAZITAET) {
                     this.addAutoAnPosition(EINFAHRTWARTEBEREICHX, EINFAHRTWARTEBEREICHY, this.posEinfahrt % 10, this.posEinfahrt / 10);
                     this.posEinfahrt++;
                 }
                 break;
             case "A":
-                if (this.posAusfahrt < ANZAHLAUTOSAUSFAHRT) {
+                if (this.posAusfahrt < AUSFAHRTKAPAZITAET) {
                     this.addAutoAnPosition(AUSFAHRTWARTEBEREICHX, AUSFAHRTWARTEBEREICHY, this.posAusfahrt % 10, this.posAusfahrt / 10);
                     this.posAusfahrt++;
                 }
                 break;
             case "B":
-                if (this.posBaustelle < ANZAHLAUTOSBAUSTELLE) {
+                if (this.posBaustelle < BAUSTELLEKAPAZITAET) {
                     this.addAutoAnPosition(BAUSTELLENBEREICHX, BAUSTELLENBEREICHY, this.posBaustelle / 10, this.posBaustelle % 10);
                     this.posBaustelle++;
                 }

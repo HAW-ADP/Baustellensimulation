7d00ddfe8e98a746edf99891ec9aaa57ba096148
Luciano Maiwald <luciano.maiwald@gmail.com>
2012-05-08 15:56:44 +0200

    anwendungsbeispiel in simulation umbenannt

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -3,8 +3,6 @@ package gui;
 import gui.logik.*;
 import java.util.PriorityQueue;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import processing.core.PApplet;
 import processing.core.PImage;
 
